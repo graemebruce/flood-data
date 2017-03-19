5b438725347c55bf3b00abf96fbc44b6de1f9082
graemebruce <grjbruce@gmail.com>
2017-03-19 14:22:19 -0500

    Update scraping.js
    Updates code

--- scraping.js
+++ scraping.js
@@ -4,54 +4,77 @@ var path = require('path');
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
-
-
+var moment = require('moment');
 //local server stuff
 var app = express();
 var port = 8000;
-
 //empty array for results
 var data = [];
-
 //csv to place data
 var outputFile = 'water_levels.csv';
-
 //
 // var destination = fs.createWriteStream("./downloads/riverlevels6.csv");
-
 //url to scrape
 var url = "http://www.winnipeg.ca/publicworks/pwddata/riverlevels/"
-
-// cron.schedule('*/1 * * * *', function(){
+  // cron.schedule('*/1 * * * *', function(){
   //select table data
 request(url, function(err, resp, body) {
-  var $ = cheerio.load(body);
-  var level = $('table[name=RED] tr:nth-child(6) td:nth-child(3)');
-  var time = $('table[name=RED] tr:nth-child(6) td:nth-child(4)');
+    var $ = cheerio.load(body);
+    var level = $('table[name=RED] tr:nth-child(6) td:nth-child(2)');
+    var time = $('table[name=RED] tr:nth-child(6) td:nth-child(4)');
+    //turn it into text
+    var leveltext = level.text();
+    var timetext = time.text();
 
-  //turn it into text
-  var leveltext = level.text();
-  var timetext = time.text();
+
+
+
+// parse the weird city date/time format
+var dateParse = new Date(timetext);
+
+
+//turn Unix Epoch time string into human numbers
+var theDay = dateParse.getDate(),
+    theMonth = dateParse.getMonth() + 1,
+    theHour = dateParse.getHours(),
+    theMinutes = dateParse.getMinutes(),
+    theSeconds = dateParse.getSeconds();
+
+
+
+
+  
+   
+
+
+    console.log(theMinutes);
+
+
+
+var timeOfDay = theHour +':' + theMinutes;
+var date = theDay + '/' + theMonth
+
+
+console.log(date, timeOfDay);
+
+
+
+  
+
 
 
 
   //concantenate level, time
-  data = [leveltext, timetext, '\r\n'];
-
-  fs.appendFile(outputFile, data, 'utf8', function (err) {
-  if (err) {
-    console.log('Some error occured - file either not saved or corrupted file saved.');
-  } else {
-    console.log('It is saved!');
-  }
+data = ['2017',date, leveltext, '\r\n'];
+fs.appendFile(outputFile, data, 'utf8', function(err) {
+if (err) {
+  console.log('Some error occured - file either not saved or corrupted file saved.');
+} else {
+  console.log('It is saved!');
+}
 });
-
-  //push to empty array
-  // entries.push(data);
-
-
-  // console.log(entries)
+//push to empty array
+// entries.push(data);
+// console.log(entries)
 })
 // });
-
-
